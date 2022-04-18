22aa30fbb8f215ee453c37510478b55949ed5528
Divya <dkhiani66@gmail.com>
2022-04-18 16:02:39 -0700

    adding interfaces

---
+++ HotelBooking/src/main/java/com/spts/interfaces/IUserRecord.java
@@ -0,0 +1,15 @@
+package com.spts.interfaces;
+
+import java.util.List;
+
+import com.spts.signup.User;
+
+/**
+ *  @author Teja Ganapati Jaddipal - 015957526
+ */
+
+public interface IUserRecord {
+	
+	public abstract int addNewUserRecord(User user);
+
+}
